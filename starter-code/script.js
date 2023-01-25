c98cad319a54357075dbe886cbdeb652d922177c
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2023-01-25 22:05:09 +0100

    full script.js, breakpoints for tablet and desktop

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,53 +1,94 @@
 const input = document.querySelector('.header__input')
 const dictionaryWord = document.querySelector('.dictionary__word');
 const dictionaryPhonetics = document.querySelector('.dictionary__phonetics')
-const nounList = document.querySelector('.dictionary__list-noun');
-const verbList = document.querySelector('.dictionary__list-verb');
-const adjectiveList = document.querySelector('.dictionary__list-adjective');
-const adjectiveElement = document.querySelector('.adjective');
-const verbElement = document.querySelector('.verb');
-const nounElement = document.querySelector('.noun');
 const source = document.querySelector('.dictionary__link')
+const sourceTitle = document.querySelector('.dictionary__source')
+const sourceImg = document.querySelector('.dictionary__source-img')
 const playButton = document.querySelector('.dictionary__play')
-const verbSynonyms = document.querySelector('.verb-synonyms')
-const nounSynonyms = document.querySelector('.noun-synonyms')
-const adjectiveSynonyms = document.querySelector('.adjective-synonyms')
-const synonyms = document.querySelectorAll('.dictionary__synonyms')
+const parent = document.querySelector('.dictionary__parentElement')
+const audio = document.querySelector('.audio')
+const fontSelect = document.querySelector('.header__fonts')
+const body = document.querySelector('body')
+const toggleBtn = document.querySelector('#switch');
+const inputWrapper = document.querySelector('.input-wrapper')
+const header = document.querySelector('.dictionary__head')
+const logo = document.querySelector('.header__logo')
 
+//function for fonts changing
+fontSelect.addEventListener('change', function() {
+ const addFont1 = () => {
+    body.classList.add('font1')
+    body.classList.remove('font2')
+    body.classList.remove('font3')
+ }
+ const addFont2 = () => {
+    body.classList.add('font2')
+    body.classList.remove('font1')
+    body.classList.remove('font3')
+ }
+ const addFont3 = () => {
+    body.classList.add('font3')
+    body.classList.remove('font1')
+    body.classList.remove('font2')
+ } 
+if (this.value === 'Mono') addFont1();
+if (this.value === 'Sans Serif') addFont2();
+if (this.value === 'Serif') addFont3();
+  });
+// toggle for dark mode
+const toggle = () => {
+    toggleBtn.checked ? (body.classList.add('dark'))
+    : body.classList.remove('dark');
+}
+toggleBtn.oninput = toggle
+
+//if toggle is on dark, switch to dark
+toggleBtn.checked ? body.classList.add('dark') : null
+//function for reset
+function clearInput() {
+    dictionaryWord.innerText = '';
+    dictionaryPhonetics.innerText = '';
+    source.innerText = '';
+    input.value = '';
+}
+//reload when press logo
+const reloadOnLogo = () => {
+    window.location = window.location
+}
+logo.addEventListener('click', reloadOnLogo)
 const catchWord = (e) => {
     e.preventDefault();
     if (e.keyCode === 13) {
-        const inputValue = input.value
-    // Clear any existing data
-         dictionaryWord.innerText = ''
-         dictionaryPhonetics.innerText = ''
-         source.innerText = ''
-         input.value = ''
-         source.setAttribute('href', '');
-         while (nounList.firstChild) {
-             nounList.removeChild(nounList.firstChild);
-         }
-         while (verbList.firstChild) {
-             verbList.removeChild(verbList.firstChild);
-         }
-         while (adjectiveList.firstChild) {
-             adjectiveList.removeChild(adjectiveList.firstChild);
-         }
-
-         nounElement.style.display = 'none'
-         verbElement.style.display = 'none'
-         adjectiveElement.style.display = 'none'
-         nounSynonyms.innerHTML = ''
-         verbSynonyms.innerHTML = ''
-         adjectiveSynonyms.innerHTML = ''
-        
-        
+    const resetParent = () => {
+     while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+            }
+        }
+    const inputValue = input.value
+    if (!inputValue){
+    return
+    } else {
+        resetParent(); clearInput()
+            }
         axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
   .then(response => {
-    let { word, phonetic, sourceUrls, meanings } = response.data[0];
+    let { word, phonetic, phonetics, sourceUrls, meanings } = response.data[0];
+
+    const openSourceInNewWindow = () => {
+        return window.open(source.innerText)
+    }
+    sourceImg.addEventListener('click', openSourceInNewWindow)
     // main word
     dictionaryWord.innerText = word
 
+    // deleting error message when everything is ok
+    if(document.querySelector('.dictionary__error') !== null) {
+        inputWrapper.removeChild(document.querySelector('.dictionary__error'));
+        header.removeChild(document.querySelector('.dictionary__img-wrapper'));
+        playButton.style.display = "block"
+        sourceTitle.style.display = "block"
+        sourceImg.style.display = "block"
+    }
     // making url
     source.innerText = sourceUrls
     source.setAttribute('href', sourceUrls);
@@ -57,73 +98,133 @@ const catchWord = (e) => {
     if (dictionaryPhonetics.innerText === 'undefined') {
         dictionaryPhonetics.innerText = ''
     }
-// const play = () => {
-
-//     let audio = response.data[0].phonetics[1].audio
-//     if (response.data[0].phonetics[2].audio !== undefined) {
-//         audio = new Audio(response.data[0].phonetics[2].audio);
-//     } else {
-//         audio = new Audio(response.data[0].phonetics[0].audio);
-//     }
-//     console.log(audio)
-
-//     audio.play();
-// }
-// playButton.addEventListener('click', play)
-console.log(synonyms)
-//getting synonims from json
-const synonymMaker = (number, div, place) => {
-    //if object in json exist set display block
-    if (meanings[number] && meanings[number].synonyms) { 
-        div.style.display = "block";
-        //if object in json exist, first make a title
-        if (meanings[number].synonyms.length > 0) {
-          const title = document.createElement('p');
-          title.setAttribute('class', 'dictionary__synonym-name');
-          place.appendChild(title);
-          title.innerText = "Synonyms";
+    phonetics.forEach(data => {
+        if (!data.audio || audio.src === '/starter-code/500-milliseconds-of-silence.mp3'){
+            playButton.style.filter = "grayscale(100%)";
+            playButton.style.cursor = "default";
+        } else {
+            playButton.style.filter = "grayscale(0%)";
+            playButton.style.cursor = "pointer";
         }
-        //create list to push synonyms
-        const list = [];
-        for (const synonyms of meanings[number].synonyms) {
-          list.push(synonyms)
-        }
-        //if synonyms in list exist make DOM element
-        if (list.length > 0) {
-          const element = document.createElement('p');
-          element.setAttribute('class', 'dictionary__synonym');
-          place.appendChild(element);
-          element.innerText = list.join(', '); 
-        }
-        //if not exist set display to none
-    } else {
-        div.style.display = "none";
+    });
+// audio
+    const playAudio = () => {
+    audio.pause();
+    audio.currentTime = 0;
+    playButton.style.cursor = 'pointer'
+    audio.src = '/starter-code/500-milliseconds-of-silence.mp3'; 
+    if (phonetics[0].audio){
+        audio.src = phonetics[0].audio;
+    } else if (audio.src === phonetics[1].audio){
+        audio.src = phonetics[1].audio
+    } else if (audio.src === phonetics[2].audio){
+        audio.src = phonetics[2].audio
     }
+    else
+    playButton.style.cursor = "default";
+    audio.play();
+    
 }
-//function ignition for synonyms
-synonymMaker(2, adjectiveSynonyms, adjectiveSynonyms)
-synonymMaker(1, verbSynonyms, verbSynonyms);
- synonymMaker(0, nounSynonyms, nounSynonyms)
- 
-// function for making noun and verb lists
-const definitionMaker = (number, list, div) => {
-    for (const [key, value] of Object.entries(meanings[number].definitions)) {
-        div.style.display = "block"
-        const element = document.createElement('li');
-        element.setAttribute('class', 'dictionary__definition');
-        list.appendChild(element)
-        element.innerText = value.definition}  
-}
-definitionMaker(0, nounList, nounElement); 
-definitionMaker(1, verbList, verbElement);
-definitionMaker(2, adjectiveList, adjectiveElement);
+    meanings.forEach(data => {
+        //wrapper of content
+        const partOfSpeechDiv = document.createElement('div');
+        partOfSpeechDiv.setAttribute('class', `title-wrapper`)
+        parent.appendChild(partOfSpeechDiv)
 
+        //making wrapper for title
+        const partOfSpeechElement = document.createElement('div');
+        partOfSpeechElement.setAttribute('class', `dictionary__partOfSpeech-element`)
+        partOfSpeechDiv.appendChild(partOfSpeechElement)
 
+        //making title with line
+        const partofSpeechTitle = document.createElement('div');
+        partofSpeechTitle.setAttribute('class', `dictionary__partOfSpeech`);
+        const line = document.createElement('div');
+        line.setAttribute('class', `dictionary__line`);
 
+        //add title to DOM
+        partOfSpeechElement.appendChild(partofSpeechTitle);
+        partOfSpeechElement.appendChild(line)
+        //inner text of title to json.data
+        partofSpeechTitle.innerText = data.partOfSpeech;
+
+        // add word 'meaning' to DOM
+        const meaningText = document.createElement('div');
+        meaningText.setAttribute('class', `dictionary__meaning`)
+        partOfSpeechDiv.appendChild(meaningText)
+        meaningText.innerText = 'Meaning'
+
+        // list for definitions
+        const definitonslist = document.createElement('ul')
+        definitonslist.setAttribute('class', 'dictionary__list')
+        partOfSpeechDiv.appendChild(definitonslist)
+
+        data.definitions.forEach(object => {
+        //adding definitions to lists
+        const definitonsText = document.createElement('li');
+        definitonsText.setAttribute('class', 'dictionary__definition');
+        definitonslist.appendChild(definitonsText)
+        definitonsText.innerText = object.definition
+        const definitonExample = document.createElement('p');
+        definitonExample.setAttribute('class', 'dictionary__example');
+        definitonsText.appendChild(definitonExample)
+        //if definition is not undefined, add definiton to innerText
+        if (object.example !== undefined) {
+            definitonExample.innerText = `"${object.example}"`
+        }
+        }  )
+        //function of making antonyms and synonyms with title
+    const synonymAntonymMaker = (kindOf, titleOf) => {
+        let list = [];
+        //creating synonyms element
+        const element = document.createElement('p');
+        element.setAttribute('class', 'dictionary__synonyms-antonyms');
+        const title = document.createElement('p');
+        //creating synonyms title
+        title.setAttribute('class', 'dictionary__synonyms-antonyms-title');        
+        definitonslist.appendChild(title);
+        data[kindOf].forEach(object => {
+        list.push(object);
+        if (element) {
+        title.innerText = `${titleOf}`;
+        }
+        });
+        
+        element.innerText = list.join(', ');
+        definitonslist.appendChild(element);
+    }
+    synonymAntonymMaker('synonyms', 'Synonyms')
+    synonymAntonymMaker('antonyms', 'Antonyms')
+    }); 
+playButton.addEventListener('click', playAudio)
   })
+
+  //getting error
   .catch(error => {
-    console.log(error);
+    //if there is not error
+    if (!document.querySelector('.dictionary__error')) {
+        //create error elements and remove DOM elements
+        const msg = document.createElement('p');
+        sourceTitle.style.display = "none"
+        sourceImg.style.display = "none"
+        msg.setAttribute('class', 'dictionary__error');
+        inputWrapper.appendChild(msg)
+        msg.innerText = 'Please write the correct word';
+        const errorImg = document.createElement('img');
+        errorImg.setAttribute('src', '/starter-code/assets/images/404-error.png');
+        errorImg.setAttribute('class', 'dictionary__errorImg');
+        // header.appendChild(errorImg)
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'dictionary__img-wrapper');
+        wrapper.appendChild(errorImg)
+        header.appendChild(wrapper)
+        
+        playButton.style.display = "none"
+
+    }
   });
     }
+
 }
 input.addEventListener('keyup', e => catchWord(e))
+
